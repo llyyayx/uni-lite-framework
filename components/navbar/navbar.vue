91940933e73b52daf8200c951a71c39fc5b1b01b
llyyayx <2235001053@qq.com>
2026-01-14 15:28:06 +0800

    fix:修复navbar

--- components/navbar/navbar.vue
+++ components/navbar/navbar.vue
@@ -1,33 +1,34 @@
 <template>
-	<uv-navbar
-		:leftText="leftText"
-		:title="title"
-		:bgColor="bgColor"
-		:fixed="fixed"
-		:safeAreaInsetTop="true"
-		:titleStyle="{
-			color: textColor,
-			fontSize: titleSize,
-		}"
-		class="custom__navbar"
-		:style="{'--textColor': textColor}"
-	>
-		<template v-slot:left>
-			<view class="custom__navbar-slot">
-				<uv-icon name="arrow-left" size="20" :color="textColor" @click="goBack"></uv-icon>
-				<uv-line
-					direction="column"
-					:hairline="false"
-					length="16"
-					margin="0 16rpx"
-				></uv-line>
-				<uv-icon name="home" size="24" :color="textColor" @click="goHome"></uv-icon>
-			</view>
-		</template>
-		<template v-slot:right>
-			<slot name="right"></slot>
-		</template>
-	</uv-navbar>
+	<view class="navbar__container" :style="{'--textColor': textColor}">
+		<uv-navbar
+			:leftText="leftText"
+			:title="title"
+			:bgColor="bgColor"
+			:fixed="fixed"
+			:safeAreaInsetTop="true"
+			:titleStyle="{
+				color: textColor,
+				fontSize: titleSize,
+			}"
+			class="custom__navbar"
+		>
+			<template v-slot:left>
+				<view class="custom__navbar-slot">
+					<uv-icon name="arrow-left" size="20" :color="textColor" @click="goBack"></uv-icon>
+					<uv-line
+						direction="column"
+						:hairline="false"
+						length="16"
+						margin="0 16rpx"
+					></uv-line>
+					<uv-icon name="home" size="24" :color="textColor" @click="goHome"></uv-icon>
+				</view>
+			</template>
+			<template v-slot:right>
+				<slot name="right"></slot>
+			</template>
+		</uv-navbar>
+	</view>
 </template>
 
 <script setup>
@@ -93,21 +94,22 @@ const goHome = () => {
 </script>
 
 <style lang="scss" scoped>
-.custom__navbar {
-	height: 88rpx;
-	:deep(.uv-navbar--fixed) {
-		z-index: 99999;
+.navbar__container {
+	.custom__navbar {
+		:deep(.uv-navbar--fixed) {
+			z-index: 99999;
+		}
 	}
-}
-.custom__navbar-slot {
-	display: flex;
-	align-items: center;
-	justify-content: space-between;
-	border-width: 0.5px;
-	border-style: solid;
-	border-radius: 200rpx;
-	border-color: var(--textColor);
-	padding: 6rpx 14rpx;
-	opacity: 0.8;
+	.custom__navbar-slot {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		border-width: 0.5px;
+		border-style: solid;
+		border-radius: 200rpx;
+		border-color: var(--textColor);
+		padding: 6rpx 14rpx;
+		opacity: 0.8;
+	}
 }
 </style>
